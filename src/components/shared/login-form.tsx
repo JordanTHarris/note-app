6e12fdfcdb1ee3a319f81b6c5a2a3c04270ac362
Jordan Harris <jordanharris1175@gmail.com>
2024-05-12 23:37:27 -0400

    Add Google as an auth provider

--- src/components/shared/login-form.tsx
+++ src/components/shared/login-form.tsx
@@ -3,15 +3,40 @@
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { Bars } from "react-loader-spinner";
-import { Discord } from "@/components/shared/icons";
+import { Discord, Google } from "@/components/shared/icons";
 import { Button } from "@/components/ui/button";
 
 export const LoginForm = () => {
+  const [googleLoading, setGoogleLoading] = useState(false);
   const [discordLoading, setDiscordLoading] = useState(false);
-  const color = "#5865F2";
+  const googleColor = "#5865F2";
+  const discordColor = "#5865F2";
 
   return (
     <div className="flex items-center justify-center">
+      <Button
+        disabled={googleLoading}
+        variant="ghost"
+        className="flex h-32 w-32 items-center justify-center space-x-3 rounded-full p-2 transition-all duration-75 focus:outline-none disabled:opacity-100"
+        size="icon"
+        onClick={() => {
+          setGoogleLoading(true);
+          void signIn("google");
+        }}
+      >
+        {googleLoading ? (
+          // <LoadingDots className="bg-primary" />
+          <Bars height="80" width="80" color={discordColor} />
+        ) : (
+          <div className="flex flex-1 flex-col items-center justify-center gap-2">
+            <Google className="h-20 w-20" />
+            <div
+              className="h-1 w-16 animate-pulse"
+              style={{ backgroundColor: googleColor }}
+            />
+          </div>
+        )}
+      </Button>
       <Button
         disabled={discordLoading}
         variant="ghost"
@@ -24,11 +49,14 @@ export const LoginForm = () => {
       >
         {discordLoading ? (
           // <LoadingDots className="bg-primary" />
-          <Bars height="80" width="80" color={color} />
+          <Bars height="80" width="80" color={discordColor} />
         ) : (
           <div className="flex flex-1 flex-col items-center justify-center gap-2">
             <Discord className="h-20 w-20" />
-            <div className="h-1 w-16 animate-pulse" style={{ backgroundColor: color }} />
+            <div
+              className="h-1 w-16 animate-pulse"
+              style={{ backgroundColor: discordColor }}
+            />
           </div>
         )}
       </Button>
--- src/env.js
+++ src/env.js
@@ -8,22 +8,20 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
+      process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -46,6 +44,8 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
